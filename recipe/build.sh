f89b9c3b545118afc7ea3f77330397d14bdd1409
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2023-10-02 13:28:32 -0400

    upadted to 3.0.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,1 @@
-$SRC_DIR/install.sh "$PREFIX"
-
-# Remove Google Test from the package to avoid clashes
-rm "$PREFIX/lib/libgtest.a" "$PREFIX/lib/libgtest_main.a" "$PREFIX/lib/libgmock.a" "$PREFIX/lib/libgmock_main.a"
-rm -r "$PREFIX/include/gtest/" "$PREFIX/include/gmock/"
-
-echo "Making a test build including all headers"
-cd $SRC_DIR/build
-make compile-test
+cp -r $SRC_DIR/include/sdsl $PREFIX/include/
